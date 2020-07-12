67a9e9740230c7fa9dd2b62e9f85b6895e611196
Danvir Sehmi <DSehmi@spartaglobal.com>
2020-07-12 18:15:41 +0100

    Final changes to FizzBuzz Assignment

--- Introduction/fizzbuzz.py
+++ Introduction/fizzbuzz.py
@@ -82,3 +82,25 @@
 #         print(buzz)
 #     else:
 #         print(number)
+
+
+# # 5. Custom values for fizz and buzz
+
+# start = int(input("What number would you like to start from?\n"))
+# end = int(input("What number would you like to end on?\n"))
+# fizz_value = int(input("What fizz number would you like?\n"))
+# buzz_value = int(input("What buzz number would you like?\n"))
+# fizz = input("Fizz is pretty boring, what word would you prefer to use?\n")
+# buzz = input("Buzz isn't actually that bad, but I know we can still do better...\n")
+# increment = int(input("What increment value would you like to have?\n"))
+# 
+#
+# for number in range(start, end+1, increment):
+#     if number % fizz_value == 0 and number % buzz_value == 0:
+#         print(fizz+buzz)
+#     elif number % fizz_value == 0:
+#         print(fizz)
+#     elif number % buzz_value == 0:
+#         print(buzz)
+#     else:
+#         print(number)
