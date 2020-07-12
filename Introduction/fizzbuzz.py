db471b0c887c3298f3232ea99dd1734c6eca1166
Danvir Sehmi <DSehmi@spartaglobal.com>
2020-07-12 17:32:26 +0100

    Completed FizzBuzz Assignment

--- Introduction/fizzbuzz.py
+++ Introduction/fizzbuzz.py
@@ -9,11 +9,67 @@
 # If divisible by both, instead of number, "FizzBuzz"
 
 # EXTRA
-# Accept user input for number to count up to?
-# Accept user input for number to start from?
-# Accept user input for custom fizz buzz?
-# Change the number we increment by
+# Accept user input for number to count up to? DONE
+# Accept user input for number to start from? DONE
+# Accept user input for custom fizz buzz? DONE
+# Change the number we increment by. DONE
 # Change values for fizz and buzz
 
 # PEP8 = style guide for python. Read link sent.
 
+# for number in range(1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("FizzBuzz!")
+#     elif number % 3 == 0:
+#         print("Fizz!")
+#     elif number % 5 == 0:
+#         print("Buzz!")
+#     else:
+#         print(number)
+
+# # custom user start and end values
+# start = int(input("What number would you like to start from?\n"))
+# end = int(input("What number would you like to end on?\n"))
+#
+# for number in range(start, end + 1):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("FizzBuzz!")
+#     elif number % 3 == 0:
+#         print("Fizz!")
+#     elif number % 5 == 0:
+#         print("Buzz!")
+#     else:
+#         print(number)
+
+# # custom user fizz and buzz words
+# start = int(input("What number would you like to start from?\n"))
+# end = int(input("What number would you like to end on?\n"))
+# fizz = input("Fizz is pretty boring, what word would you prefer to use?\n")
+# buzz = input("Buzz isn't actually that bad, but I know we can still do better...\n")
+#
+# for number in range(start, end + 1):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print(fizz + buzz)
+#     elif number % 3 == 0:
+#         print(fizz)
+#     elif number % 5 == 0:
+#         print(buzz)
+#     else:
+#         print(number)
+
+# # custom user increment value
+# start = int(input("What number would you like to start from?\n"))
+# end = int(input("What number would you like to end on?\n"))
+# fizz = input("Fizz is pretty boring, what word would you prefer to use?\n")
+# buzz = input("Buzz isn't actually that bad, but I know we can still do better...\n")
+# increment = int(input("What increment value would you like to have?\n"))
+#
+# for number in range(start, end + 1, increment):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print(fizz + buzz)
+#     elif number % 3 == 0:
+#         print(fizz)
+#     elif number % 5 == 0:
+#         print(buzz)
+#     else:
+#         print(number)
