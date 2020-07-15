26d93f82db0038b922960af4180d0b8c32b4d971
Danvir Sehmi <DSehmi@spartaglobal.com>
2020-07-15 12:20:31 +0100

    Pillars of OOP example

---
+++ OOP/pillars_of_OOP.py
@@ -0,0 +1,62 @@
+class Animal:
+    def __init__(self, name, legs):
+        self.name = name
+        self.legs = legs
+        self._hunger = 5
+        print("I exist.")
+
+    def breathe(self):
+        print("Breathing in...")
+        print("Breathing out...")
+        self._hunger += 0.1
+
+    def eat(self):
+        print("Nom nom nom")
+        self._hunger = 0
+
+
+class Mammal(Animal):
+    def __init__(self, name, colour):
+        super().__init__(name, 4)
+        self.colour = colour
+
+    def give_birth(self):
+        print("I have given live birth")
+
+
+class Dog(Mammal):
+    def __init__(self, name, colour):
+        super().__init__(name, colour)
+
+    def wag_tail(self):
+        print("Swish swish")
+        super().breathe()
+        self._hunger += 1
+
+
+class Chihuahua(Dog):
+    def feisty(self):
+        print("I am angry all the time. I may be small but don't mess with me!!!")
+
+
+class Bat(Mammal):
+    def __init__(self, name, colour):
+        super().__init__(name, colour)
+        self.legs = 2
+
+
+class Platypus(Mammal):
+    def __init__(self, name, colour):
+        super().__init__(name, colour)
+
+    def give_birth(self):
+        print("I have laid an egg")
+
+
+my_bat = Bat("Bruce", "Black")
+print(my_bat.give_birth())
+my_platypus = Platypus("Perry", "Blue")
+print(my_platypus.give_birth())
+
+# Avoid having long inheritance chains
+
